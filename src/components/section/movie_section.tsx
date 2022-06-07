43ec652041026f88dc75d0e0e53ef64f975cef3a
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-06-07 15:49:16 +0300

    Update movie_section.tsx

--- src/components/section/movie_section.tsx
+++ src/components/section/movie_section.tsx
@@ -1,64 +1,57 @@
 import React, { useEffect, useState } from "react";
-import axios from "../axios";
-import "./moviesection.css";
-import { Movie } from "../../types";
-import { useNavigate } from "react-router-dom";
+import axios from  "../axios"
+import './moviesection.css';
+import {Movie} from '../../types'
+import {useNavigate} from "react-router-dom"
 import { useMovie } from "../../context/contextProvider";
 
-interface Props {
-  title: string;
-  fetchUrl: string;
-  isLarge?: boolean;
+interface Props{
+  title: string,
+  fetchUrl: string,
+  isLarge?: boolean,
 }
 
-const MovieSection = (props: Props) => {
-  const [movies, setMovies] = useState<Movie[]>([]);
 
-  useEffect(() => {
-    axios
-      .get(props.fetchUrl)
-      .then((response) => {
+const MovieSection = (props: Props) =>{
+    const [movies, setMovies] = useState<Movie[]>([]);
+   
+    useEffect(()=>{
+      axios.get(props.fetchUrl).then(response => {
         setMovies(response.data.results);
         console.log(response.data.results);
-      })
-      .catch((error) => {
-        console.log(error);
+        
+      }).catch(error =>{
+         console.log(error);
       });
-  }, [props.fetchUrl]);
 
-  const { setMovieId } = useMovie();
 
-  const imageBaseUrl: string = "https://image.tmdb.org/t/p/w500/";
+    },[props.fetchUrl])
 
-  const navigate = useNavigate();
 
-  const findMovie = (id: number | undefined) => {
-    setMovieId(id);
-    navigate("/movie");
-  };
+    const {movieId, setMovieId} = useMovie()
 
-  return (
-    <div className="moviesection ">
-      <h4 data-testid="heading">{props.title}</h4>
+    const imageBaseUrl:string = 'https://image.tmdb.org/t/p/w500/';
+  
+     const navigate = useNavigate();   
+    
+    const findMovie = (id: number | undefined) =>{
+         setMovieId(id)   
+       navigate('/movie')
+    }
 
-      <div
-        className={`moviesection__poster ${
-          props.isLarge && "moviesection_large"
-        }`}
-      >
-        {movies.map((movie) => (
-          <img
-            src={` ${imageBaseUrl}${
-              props.isLarge ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-            key={movie.id}
-            onClick={() => findMovie(movie.id)}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
+   return(
+       <div className= 'moviesection '>
+         <h4 data-testid="heading">{props.title}</h4>
+
+         <div className= {`moviesection__poster ${props.isLarge && "moviesection_large"}`}>
+           {
+               movies.map(movie => <img src= {` ${imageBaseUrl}${props.isLarge ? movie.poster_path : movie.backdrop_path}` } alt ={movie.name} key= {movie.id} onClick={()=>findMovie(movie.id)} />)
+           }
+         </div>
+
+       </div>
+   );
+}
+
 
 export default MovieSection;
