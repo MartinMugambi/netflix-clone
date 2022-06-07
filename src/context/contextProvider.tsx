8ae40daa9c7a2b869066e1c2cf2e7db5fb32f4f6
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-06-07 10:31:29 +0300

    Update contextProvider.tsx

--- src/context/contextProvider.tsx
+++ src/context/contextProvider.tsx
@@ -1,10 +1,17 @@
 import { useState, createContext, useContext } from "react";
+import { Movie } from "../types";
 
 export const StateContext = createContext<number | any>(0);
 const ContextProvider = ({ children }: any) => {
   const [movieId, setMovieId] = useState(0);
+  const [movies, setMovies] = useState<Movie>();
+  const [seconds, setSeconds] = useState<number>(0);
   return (
-    <StateContext.Provider value={{movieId, setMovieId}}>{children}</StateContext.Provider>
+    <StateContext.Provider
+      value={{ movieId, setMovieId, movies, setMovies, seconds, setSeconds }}
+    >
+      {children}
+    </StateContext.Provider>
   );
 };
 
